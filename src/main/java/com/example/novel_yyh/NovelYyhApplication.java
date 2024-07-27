18857f5409d1b136c70ed94f4f713f703caca9a4
xinxuefanxuan <2809832579@qq.com>
2024-07-27 15:07:36 +0800

    初始化项目

---
+++ src/main/java/com/example/novel_yyh/NovelYyhApplication.java
@@ -0,0 +1,13 @@
+package com.example.novel_yyh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovelYyhApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NovelYyhApplication.class, args);
+    }
+
+}
